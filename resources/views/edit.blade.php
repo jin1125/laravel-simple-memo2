6b585224a67ce582ad7cac6f8e4c3a3cf4cf1942
jinta <shogo.jinta@qnote.co.jp>
2022-04-24 18:58:49 +0900

    42

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,17 +6,23 @@
             memo edit
             <form class="card-body" action="{{ route('destory') }}" method="POST">
                 @csrf
-                <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
+                <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}">
                 <button type="submit">削除</button>
             </form>
         </div>
         <div class="card-body">
             <form class="card-body" action="{{ route('update') }}" method="POST">
                 @csrf
-                <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
+                <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}">
                 <div class="form-group">
-                    <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo['content'] }}</textarea>
+                    <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力">{{ $edit_memo[0]['content'] }}</textarea>
                 </div>
+                @foreach($tags as $t)
+                    <div class="form-check form-check-inline mb-3">
+                    <input class="form-check-input" type="checkbox" name="tags[]" id="{{ $t['id'] }}" value="{{ $t['id'] }}" {{ in_array($t['id'], $include_tags) ? 'checked' : '' }}>
+                    <label class="form-check-label" for="{{ $t['id'] }}">{{ $t['name']}}</label>
+                    </div>
+                @endforeach
                 <button type="submit" class="btn btn-primary">更新</button>
             </form>
         </div>
