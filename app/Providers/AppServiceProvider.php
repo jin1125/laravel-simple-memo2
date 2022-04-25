19cd4f43faad827dc611c485189928f0f3f35fa5
jinta <shogo.jinta@qnote.co.jp>
2022-04-25 10:43:29 +0900

    47

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,11 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('*', function($view) {
-            $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at', 'DESC')
-            ->get();
+            $memo_model = new Memo();
+
+            $memos = $memo_model->getMyMemo();
 
             $tags = Tag::where('user_id', '=', \Auth::id())
             ->whereNull('deleted_at')
